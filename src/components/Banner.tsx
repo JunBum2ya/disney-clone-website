e600ff1201847b635722b30ea46c901c801ab258
DESKTOP-FK2FMAS\22cun <22cun2@naver.com>
2024-02-05 16:05:18 +0900

    add banner
    이미지 배너 추가

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,3 +1,55 @@
-const Banner = () => {};
+import axios from '../api/axios';
+import { useEffect, useState } from 'react';
+import { requests } from '../api/requests';
+import '../scss/Banner.scss';
+
+const Banner = () => {
+  const [movie, setMovie] = useState<any>([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const response = await axios.get(requests.fetchNowPlaying);
+    const movieId =
+      response.data.results[
+        Math.floor(Math.random() * response.data.results.length)
+      ].id;
+    const { data: movieDetail } = await axios.get(`movie/${movieId}`, {
+      params: { append_to_response: 'videos' },
+    });
+    setMovie(movieDetail);
+  };
+
+  return (
+    <header
+      className="banner"
+      style={{
+        backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie.backdrop_path}")`,
+      }}
+    >
+      <div className="banner__contents">
+        <h1 className="banner__title">{movie.title || movie.original_title}</h1>
+        <div className="banner__buttons">
+          {movie?.videos?.results[0]?.key && (
+            <button className="banner__button play">Play</button>
+          )}
+        </div>
+        <p className="banner__description">{truncate(movie.overview, 100)}</p>
+      </div>
+      <div className="banner--fadeBottom"></div>
+    </header>
+  );
+};
+
+const truncate = (
+  param: string | null | undefined,
+  endIndex: number
+): string => {
+  const str = param ?? '';
+  console.log(str);
+  return str.length > endIndex ? `${str.substring(0, endIndex)}...` : str;
+};
 
 export default Banner;
