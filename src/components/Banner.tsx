4addab582bcf4c4dc4baa135949372e7cca4e54a
DESKTOP-FK2FMAS\22cun <22cun2@naver.com>
2024-02-05 17:51:21 +0900

    api 오류 수정
    api 수정 필요

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,16 +1,22 @@
 import axios from '../api/axios';
-import { useEffect, useState } from 'react';
+import YouTube from 'react-youtube';
+import { ReactNode, useEffect, useState } from 'react';
 import { requests } from '../api/requests';
 import '../scss/Banner.scss';
 
 const Banner = () => {
   const [movie, setMovie] = useState<any>([]);
+  const [isClicked, setIsClicked] = useState(false);
+  const changeBanner = () => {
+    setIsClicked((clicked) => !clicked);
+  };
 
   useEffect(() => {
     fetchData();
   }, []);
 
   const fetchData = async () => {
+    console.log('fetch');
     const response = await axios.get(requests.fetchNowPlaying);
     const movieId =
       response.data.results[
@@ -22,6 +28,30 @@ const Banner = () => {
     setMovie(movieDetail);
   };
 
+  return isClicked ? (
+    <VideoBanner movie={movie} changeBannerFunction={changeBanner} />
+  ) : (
+    <ImageBanner movie={movie} changeBannerFunction={changeBanner} />
+  );
+};
+
+const VideoBanner = (props: BannerProps) => {
+  const { movie, changeBannerFunction } = props;
+  return (
+    <div className="banner__container">
+      <div className="home-container">
+        <YoutubeIFrame
+          videoId={movie.videos.results[0].key}
+          classList={['youtube-banner']}
+        />
+        <button onClick={changeBannerFunction}>X</button>
+      </div>
+    </div>
+  );
+};
+
+const ImageBanner = (props: BannerProps) => {
+  const { movie, changeBannerFunction } = props;
   return (
     <header
       className="banner"
@@ -33,7 +63,12 @@ const Banner = () => {
         <h1 className="banner__title">{movie.title || movie.original_title}</h1>
         <div className="banner__buttons">
           {movie?.videos?.results[0]?.key && (
-            <button className="banner__button play">Play</button>
+            <button
+              className="banner__button play"
+              onClick={changeBannerFunction}
+            >
+              Play
+            </button>
           )}
         </div>
         <p className="banner__description">{truncate(movie.overview, 100)}</p>
@@ -43,12 +78,46 @@ const Banner = () => {
   );
 };
 
+interface BannerProps {
+  movie: any;
+  changeBannerFunction: () => void;
+}
+
+const YoutubeIFrame = (props: YoutubeIFrameProps) => {
+  const handleReady = (event: any) => {
+    event.target.playVideo();
+  };
+
+  return (
+    <YouTube
+      className={props.classList?.join(' ') ?? ''}
+      videoId={props.videoId}
+      onReady={handleReady}
+      opts={{
+        width: '100%',
+        height: '100%',
+        playerVars: {
+          autoplay: 1, // true 대신에 1을 사용
+          loop: 1, // true 대신에 1을 사용
+          rel: 0, // false 대신에 0을 사용
+          modestbranding: 1, // true 대신에 1을 사용
+          playlist: props.videoId,
+        },
+      }}
+    ></YouTube>
+  );
+};
+
+interface YoutubeIFrameProps {
+  videoId: string;
+  classList?: string[];
+}
+
 const truncate = (
   param: string | null | undefined,
   endIndex: number
 ): string => {
   const str = param ?? '';
-  console.log(str);
   return str.length > endIndex ? `${str.substring(0, endIndex)}...` : str;
 };
 
